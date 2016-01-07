c8c7ee2ebf02ad0d2d9eda24aa8c088b16b94a61
JuanMa Garrido <JuanMa.Garrido@gmail.com>
2016-01-07 19:39:43 +0100

    use of

--- js/controller.js
+++ js/controller.js
@@ -1,9 +1,25 @@
 angular.module('myControllersContainer',[])
+
+  /* MyController */
   .controller('MyController',function($scope) {
     // controller logic here
     console.log ("MyController logic...")
+
+    // $scope is shared w/ the view (piece of HTML)
+    // that uses this controller (ng-controller)
+    $scope.firstName = 'Sean';
+    $scope.lastName = 'Citizen';
+
   })
+
+  /* MyOtherController */
   .controller('MyOtherController',function($scope) {
     // my other controller logic here
     console.log ("MyOtherController logic...")
+
+    // every controller manages its own $scope
+
+    $scope.firstName = 'JuanMa';
+    $scope.lastName = 'Guitar';
+
   });
